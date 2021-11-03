1686aaf6d8594b5345cc15a3806858a1448b6e73
AAGuedes <airam.a.g.85@gmail.com>
2021-11-03 13:27:52 +0000

    Added HttpClient get and post methods

--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 import { Subject } from 'rxjs';
 
 import { Post } from './post.model';
@@ -9,13 +10,23 @@ export class PostsService {
   private posts: Post[] = [];
   private postsUpdated = new Subject<Post[]>();
 
+  constructor(private http: HttpClient) { }
+
   getPosts() {
-    return [...this.posts];
+    this.http.get<{ message: String, posts: Post[] }>('http://localhost:3000/api/posts')
+      .subscribe((postData) => {
+        this.posts = postData.posts;
+        this.postsUpdated.next([...this.posts]);
+      });
   }
 
   setPost(post: Post) {
-    this.posts.push(post);
-    this.postsUpdated.next([...this.posts]);
+    this.http.post<{ message: String }>('http://localhost:3000/api/posts', post)
+      .subscribe((responseData) => {
+        console.log(responseData.message)
+        this.posts.push(post);
+        this.postsUpdated.next([...this.posts]);
+      });
   }
 
   getPostUpdatedListener() {
