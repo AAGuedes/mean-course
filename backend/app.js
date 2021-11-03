965a31c524187b7d1e1a1334da7817ceab6032eb
AAGuedes <airam.a.g.85@gmail.com>
2021-11-03 13:24:27 +0000

    Added get and post methods

--- backend/app.js
+++ backend/app.js
@@ -1,14 +1,35 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 app.use((req, res, next) => {
-  console.log('First middleware')
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE, OPTIONS');
   next();
 });
 
-app.use((req, res, next) => {
-  res.send('Hello form express!');
+app.post("/api/posts", (req, res, next) => {
+  const post = req.body;
+  console.log(post);
+  res.status(201).json({
+    message: 'Post added succesfully'
+  });
+});
+
+app.get('/api/posts', (req, res, next) => {
+  const posts = [
+    { id: '0', title: 'First server-side Post', content: 'This content is coming from the server.' },
+    { id: '1', title: 'Second server-side Post', content: 'This content is coming from the server.' },
+  ];
+  res.status(200).json({
+    message: 'Post fetched succesfully!',
+    posts: posts
+  });
 });
 
 module.exports = app;
